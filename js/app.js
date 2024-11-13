95856852a677f539e25350508149e3e5ae54ea7a
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2024-11-13 15:12:43 +0600

    updated coding comments

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 
-// toggle 
+// header toggle button don't need this one
 let menutoggle = document.querySelector('.toggle');
 menutoggle.onclick = function () {
   menutoggle.classList.toggle('active')
 }
+
+
+
+
+
+////////////////////////// copy paste from here, ////////////////////////////
+
+
 
 document.querySelectorAll('.thumbnail').forEach(thumbnail => {
   thumbnail.addEventListener('click', function () {
